aba257e56b932d182529c45d4dd2766f5b9116a4
Praveen Kumar Rana <31088364+PraveenKumarRana@users.noreply.github.com>
2022-03-28 15:00:30 +0530

    completed L18

--- spring5webapp/src/main/java/com/example/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/com/example/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package com.example.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -12,10 +13,13 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns =@JoinColumn(name="book_id"),
             inverseJoinColumns = @JoinColumn(name="author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
@@ -55,6 +59,14 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
